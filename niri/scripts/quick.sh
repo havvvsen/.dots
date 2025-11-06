2966d6c876138d25fb120abf79e2c09ce195bfe8
Hansen <samhansen.dev@gmail.com>
2025-11-06 23:08:44 +0300

    feat(scripts: wlsunset): add day,night,off toggles

--- niri/scripts/quick.sh
+++ niri/scripts/quick.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-OPTIONS="adb connect\ncolor picker\nscreen record\nscreenshot\neasyeffects bypass\nqr code"
-CHOICE=$(echo -e "$OPTIONS" | fuzzel --dmenu -p "> " --placeholder "choose an action")
+OPTIONS="adb connect\ncolor picker\nscreen record\nscreenshot\neasyeffects bypass\nqr code\nwlsunset"
+CHOICE=$(echo -e "$OPTIONS" | fuzzel --dmenu -p "> " --placeholder "choose an action" --lines 7)
 
 case "$CHOICE" in
 "adb connect")
@@ -25,5 +25,8 @@ case "$CHOICE" in
 	;;
 "qr code")
 	~/.config/swaync/scripts/qr.sh
+	;;
+"wlsunset")
+	~/.config/niri/scripts/wlsunset.sh
 	;;
 esac
